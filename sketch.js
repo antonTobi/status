65557c451ab76cd01c01975d7664d5f31252c360
le4TC <49806801+antonTobi@users.noreply.github.com>
2023-04-05 10:52:46 +0200

    Endless game mode

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 const startTime = 30e3
 document.bgColor = "white"
 
+document.addEventListener('mousedown', function (event) {
+    if (event.detail > 1) {
+        event.preventDefault()
+    }
+}, false)
+
 class Particle {
     constructor(c, s, x, y) {
         this.c = c
@@ -39,9 +45,10 @@ function setup() {
     points = 0
 
     highscore = getItem("statusGameHighscore") || 0
+    longestStreak = getItem("statusGameLongestStreak") || 0
+    timer = Number(getItem("statusGameLastMode") || startTime)
 
     timerRunning = false
-    timer = startTime
 
     particles = []
 }
@@ -64,25 +71,30 @@ function draw() {
 
     fill("black")
 
+    let endless = timer == Infinity
+
     textSize(0.8 * R)
     text("Time", width / 6, 0.7 * R)
-    text("Score", (3 * width) / 6, 0.7 * R)
+    text(endless ? "Streak" : "Score", (3 * width) / 6, 0.7 * R)
     text("Best", (5 * width) / 6, 0.7 * R)
 
     textSize(D)
-    text((0.001 * timer).toFixed(1), width / 6, D)
+    let displayTime = endless ? "∞" : (0.001 * timer).toFixed(1)
+    text(displayTime, width / 6, D)
     text(points, (3 * width) / 6, D)
-    text(highscore, (5 * width) / 6, D)
+    text(endless ? longestStreak : highscore, (5 * width) / 6, D)
 
     translate(0, 2 * D)
     board.draw()
     if (timer == 0) {
         noStroke()
-        fill(255, 255, 255, 200)
+        fill(255, 255, 255, 220)
         rect(0, -1, 11 * D, 11 * D + 1)
         fill("black")
         textSize(D)
-        text("Game over", 11 * R, 10.8 * R)
+        text("Game over", 11 * R, 8.8 * R)
+        textSize(R)
+        text("Click board to play again", 11 * R, 10.9 * R)
     }
     translate(0, D * 12 - 1)
 
@@ -128,6 +140,12 @@ function submit(status) {
         }
         correctSound.play()
         points += 1
+        if (timer == Infinity) {
+            if (points > longestStreak) {
+                longestStreak = points
+                storeItem("statusGameLongestStreak", longestStreak)
+            }
+        }
         let p = new Particle("lime", "+1", x, R)
         particles.push(p)
         if (!boards.length) {
@@ -137,14 +155,30 @@ function submit(status) {
     } else {
         wrongSound.play()
         points -= 1
-        if (points < 0) points = 0
+        if (timer == Infinity || points < 0) points = 0
         let p = new Particle("red", "-1", x, R)
         particles.push(p)
     }
 }
 
+function toggleGameMode() {
+    if (timer == Infinity || timer == startTime || timer == 0) {
+        if (timer == Infinity) {
+            storeItem("statusGameLastMode", startTime)
+        } else {
+            storeItem("statusGameLastMode", Infinity)
+        }
+        setup()
+    }
+}
+
 function clickHandler() {
-    if (mouseY < 13 * D && timer == 0) {
+    if (mouseY < 2 * D) {
+        if (mouseX < width / 3) {
+            toggleGameMode()
+        }
+    }
+    if (mouseY > 2 * D && mouseY < 13 * D && timer == 0) {
         setup()
     }
     if (mouseY > 13 * D) {
@@ -156,6 +190,11 @@ function clickHandler() {
             submit(2)
         }
     }
+    // return false
+}
+
+function doubleClicked() {
+    return false
 }
 
 function keyPressed() {
@@ -165,7 +204,7 @@ function keyPressed() {
         submit(3)
     } else if (key == "ArrowRight") {
         submit(2)
-    } else if (key == "Enter") {
+    } else if (key == "Enter" && timer != Infinity) {
         setup()
     }
 }
